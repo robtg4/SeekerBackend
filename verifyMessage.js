1a17a762eb289df765a4f5a646b8c77ff31cf1cb
Jason Smythe <jason@smythemail.za.net>
2018-02-28 15:48:49 +0200

    It works! Using ethereumjs-utils instead of web3!

--- verifyMessage.js
+++ verifyMessage.js
@@ -1,40 +1,26 @@
-const EthUtil = require('ethereumjs-util')
+const ethUtils = require('ethereumjs-util')
 
 const verifyMessageNonce = (userAddress, nonce, signature, web3) => {
-  const nonceString = (nonce).toString(16)
-  const hashInput = '0'.repeat(64 - nonceString.length) + nonceString + web3.toHex(userAddress).slice(2)
+
+  const idString = (new web3.BigNumber(nonce)).toString(16)
+  const hashInput = '0'.repeat(64 - idString.length) + idString
   const hash = web3.sha3(hashInput, { encoding: 'hex' })
+
   // this prefix is required by the `ecrecover` builtin solidity function (other than that it is pretty arbitrary)
   const prefix = "\x19Ethereum Signed Message:\n32";
   const prefixedBytes = web3.fromAscii(prefix) + hash.slice(2)
   const prefixedHash = web3.sha3(prefixedBytes, { encoding: 'hex' })
-  // console.log(hash)
-  // // break the signature into it's components and convert to a buffer.
-  // console.log(signature)
-  // const r = EthUtil.toBuffer(signature.slice(0,66))
-  // const s = EthUtil.toBuffer('0x' + signature.slice(66,130))
-  const v = 28
-  const signatureData = EthUtil.fromRpcSig(signature)
-  // let v = EthUtil.bufferToHex(signatureData.v)
-  let r = EthUtil.bufferToHex(signatureData.r)
-  let s = EthUtil.bufferToHex(signatureData.s)
-  // const v = EthUtil.toBuffer('0x' + signature.slice(130,132))
-  // const v = EthUtil.toBuffer('0x' + signature.slice(130,132))
-  // const m = EthUtil.toBuffer('0x' + hash)
-  const m = EthUtil.toBuffer(prefixedHash)
-  // const m = EthUtil.toBuffer(hash)
-  console.log(nonce)
-  console.log(hash)
-  console.log(m)
-  // console.log(nonce, v, r, s)
-  const publicKey = EthUtil.ecrecover(m, v, r, s)
-  const recoveredAddress = '0x' + EthUtil.pubToAddress(publicKey).toString('hex')
 
-  // TODO:: these should be the same!! (Oh no...)
-  console.log(userAddress)
-  console.log(recoveredAddress)
+  const signatureData = ethUtils.fromRpcSig(signature)
+  let rutil = ethUtils.bufferToHex(signatureData.r)
+  let sutil = ethUtils.bufferToHex(signatureData.s)
+  var echashbuf = Buffer.from(prefixedHash.slice(2), 'hex')
+  var pubkey = ethUtils.ecrecover(echashbuf, signatureData.v, rutil, sutil)
 
-  return true
+  // check that the user address is the same as the recoverd address:
+  const isValidNonce = userAddress === '0x' + ethUtils.publicToAddress(pubkey).toString('hex')
+
+  return isValidNonce
 }
 
 module.exports = {
--- verifyMessage.test.js
+++ verifyMessage.test.js
@@ -1,42 +1,39 @@
-require("babel-polyfill");
+require("babel-polyfill")
 
+import { computeMintedSignature, computeNonceSignature } from './utils/tokenIssue'
 import { assert, expect } from 'chai'
-import { signNonce } from './utils/nonceSign'
 import { verifyMessageNonce } from './verifyMessage'
 const privateKeyProvider = require('./utils/privateKeyProviderEngine')
 const TestRPC = require('ganache-cli')
 const Web3 = require('web3')
-const Promise = require('bluebird');
+const Promise = require('bluebird')
 
 
-describe("verifyMessage", () => {
-  // const privKey = '0x4e6d25d39985ffd002a291323e73d819cf43da0a493a9304eac37bda6fedec5d'
+describe("verifyMessage", async () => {
+  const privKey = '0x4e6d25d39985ffd002a291323e73d819cf43da0a493a9304eac37bda6fedec5d'
   // const publicKey = '0xbd61a5620DBd79473DF9C26BD0CEa42B1ce3dc70'
   let web3
   let accounts
 
-  // beforeEach(() => {
-  // })
-
-  it('should veryfy a correctly generated signature', async () => {
+  beforeEach(async () => {
     web3 = new Web3() //Genache for now make configurable
-    // NOTE:: Following 2 lines left here only for reference:
+    // NOTE:: Following 2 lines left here only for reference: will need to use some method of importing a private key in the final version
     // const gasPayingAccount = web3.eth.accounts.privateKeyToAccount(privKey)
-    // web3.setProvider(new privateKeyProvider(privKey, 'http://localhost:8545'))
 
     web3.setProvider(TestRPC.provider())
     if (typeof web3.eth.getAccountsPromise === 'undefined') {
       Promise.promisifyAll(web3.eth, { suffix: 'Promise' });
     }
-    const accounts = await web3.eth.getAccountsPromise()
-    console.log(accounts)
-    // left here for curiosity, why the hell is this not the same as `accounts` ???
-    console.log(Object.keys(TestRPC.provider().manager.state.accounts))
+    accounts = await web3.eth.getAccountsPromise()
+  })
 
-    const nonce = Math.floor(Math.random() * 80000000)
+  it('should veryfy a correctly generated signature', async () => {
+    const {
+      prefixedHash,
+      signature,
+    } = await computeNonceSignature(1, accounts[0], web3)
 
-    const signedNonce = await signNonce(nonce, accounts[0], web3)
-
-    assert(verifyMessageNonce(accounts[0], nonce, signedNonce, web3))
+    const isCorrectSignature = verifyMessageNonce(accounts[0], 1, signature, web3)
+    assert(isCorrectSignature)
   })
 })
